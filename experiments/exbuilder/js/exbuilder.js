946365735ba038b3ce384fa6ae8c740e8260c444
kschuler <kathryn.schuler@gmail.com>
2021-04-10 07:05:47 -0400

    rename folders

---
+++ experiments/exbuilder/js/exbuilder.js
@@ -0,0 +1,101 @@
+var Exbuilder = {
+    config: {},
+    run: {},// holds the url variables 
+
+    init: function(config_file = "../config-exbuilder.json") {
+        return new Promise((resolve, reject) => {
+
+            this.getConfig(config_file)
+            .then( config => { return this.getURLVars(); })
+            .then( run => { return this.assignRandomId(); })
+            .then( id => { 
+                if (this.run) {
+                    console.log("Exbuilder initalized succesfully..."); 
+                    console.log(this.run);
+                    resolve(this.run);
+                } else {
+                    reject("Exbuilder failed to initialize... ");
+                }    
+            })
+            .catch( err => { reject(err); })
+        })
+    },
+
+    getConfig: function(config_file) {
+        return new Promise((resolve, reject) => {
+
+            fetch(config_file)
+                .then(response => { return response.json(); })
+                .then(data => { 
+                    this.config = data;
+                    resolve(data); 
+                })
+                .catch(err => { reject(err); });
+        });
+    },
+
+    getURLVars: function() {
+        return new Promise((resolve, reject) => {
+            // If browser supposrts URL search params, get them and generate the run
+            if ('URLSearchParams' in window){
+                let params = new URLSearchParams(window.location.search);
+                this.run = Object.fromEntries(params);
+                resolve(this.run);
+            } else {
+                reject(" Exbuilder cannot retrieve url params, your browser does not support URLSearchParams... ");
+            }
+        })
+    },
+
+    assignRandomId: function() {
+        return new Promise((resolve, reject) => {
+
+            if (this.config.randomid.length) {
+                this.run['randomid'] = '_' + Math.random().toString(36).substr(2, this.config.randomid.length);
+                resolve(this.run.randomid);
+            } else {
+                reject("Exbuilder cannot assign random id; length not specified in config-exbuilder.json")
+            }
+        })
+    },
+
+    insertRun: function(){
+
+        // inserts the run to the database (new db row)
+        fetch('../exbuilder/php/insert_run.php', {
+            method: 'post',
+            body: JSON.stringify(this.run),
+        })
+        .then(result => {console.log('Success:', result);})
+        .catch(error => {console.error('Error:', error);});
+    },
+
+    updateRun: function(data){
+
+        // updates the run in the database (updates the randomid's data column)
+        fetch('../exbuilder/php/update_run.php', {
+            method: 'post',
+            body: JSON.stringify( {
+                    json_data: data,
+                    randomid: this.run.randomid
+                })
+        })
+        .then(result => {console.log('Success:', result);})
+        .catch(error => {console.error('Error:', error);});
+    },
+
+    countRuns: function() {
+
+        // pass the experiment as a search param
+        let params = new URLSearchParams({experiment: this.run.experiment});
+
+        // get the runs for this experiment in the database
+        fetch('../exbuilder/php/count_runs.php?'+params)
+            .then(response => { return response.json(); })
+            .then(data => { 
+                console.log(data);
+                return data 
+            })
+            .catch(error => {console.log('Error', error)});
+    }
+}
---
+++ experiments/exbuilder/php/count_runs.php
@@ -0,0 +1,21 @@
+<?php
+  include('config.php');
+
+  // get JSON and decode to PHP array
+  //$data_array = json_decode(file_get_contents('php://input'), true);
+
+  // connect to database
+  $dbconn = pg_connect( "host=$database->host port=$database->port dbname=$database->dbname user=$database->user password=$database->password sslmode=$database->sslmode" )
+      or die ("Could not connect to database\n");
+
+  // update the runs table with the json data from the study
+  $res = pg_query_params($dbconn, "SELECT condition, COUNT(condition) FROM experiments.runs WHERE experiment = $1 AND participant != '0' GROUP BY condition;",
+    array($_GET['experiment']))
+    or die ("Could not count runs\n");
+
+  $resultArray = pg_fetch_all($res);
+  echo json_encode($resultArray);
+
+
+pg_close($dbconn);
+?>
